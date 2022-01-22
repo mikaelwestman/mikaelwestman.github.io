17cb6ee55aaa27062794bf80c5fbc7e7840f69b0
mikaelwestman <mikael@wst.mn>
2022-01-22 04:10:22 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -7,10 +7,10 @@ class Header extends HTMLElement {
     this.innerHTML = `
 		<div id="top-navigation" class="row">
 			<div class="column">
-				<a href="index.html"><h1><span class="logo-long">© Mikael Westman 2022</span> <span class="logo-short">©MW'22</span></h1></a>
+				<a href="/"><h1><span class="logo-long">© Mikael Westman 2022</span> <span class="logo-short">©MW'22</span></h1></a>
 			</div>
 			<div class="column">	
-				<a href="index.html" id="menu-active-1">Work</a>
+				<a href="/" id="menu-active-1">Work</a>
 				<a href="about.html" id="menu-active-2">About</a>
 			</div>
 		</div>
