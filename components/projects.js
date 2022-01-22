442690940e710e2bd1be61d1b10df9ae4903f9c8
mikaelwestman <mikael@wst.mn>
2022-01-22 14:41:27 +0100

    ES case study

--- components/projects.js
+++ components/projects.js
@@ -12,15 +12,15 @@ class Projects extends HTMLElement {
 					<img src="images/3d-lighter-mikael-westman.jpg" alt="3D render of lighter">
 					<span>3D explorations</span>
 				</a>
-				<a class="column" href="square-pos.html">
-					<img src="images/pos-mikael-westman-01.jpg" alt="Square Point of Sale tablet & mobile design for Android">
-					<span>Square Point of Sale</span>
+				<a class="column" href="epidemic-sound.html">
+      				<img src="images/Epidemic-Sound-Artist.jpg" alt="Epidemic Sound artist page">
+					<span>Epidemic Sound</span>
 				</a>
 			</div>
 			<div class="row">
-				<a class="column" href="square-for-restaurants.html">
-					<img src="images/square-mikael-westman-01.png" alt="Square Point of Sale tablet & mobile design for Android">
-					<span>Square for Restaurants</span>
+				<a class="column" href="square-pos.html">
+					<img src="images/pos-mikael-westman-01.jpg" alt="Square Point of Sale tablet & mobile design for Android">
+					<span>Square Point of Sale</span>
 				</a>
 				<a class="column" href="bryggmastaren.html">
 					<img src="images/bryggmastarens-mikael-westman-03.png" alt="Square Point of Sale tablet & mobile design for Android">
@@ -28,11 +28,13 @@ class Projects extends HTMLElement {
 				</a>
 			</div>
 				<div class="row">
+				<a class="column" href="square-for-restaurants.html">
+					<img src="images/square-mikael-westman-01.png" alt="Square Point of Sale tablet & mobile design for Android">
+					<span>Square for Restaurants</span>
+				</a>
 				<a class="column" href="oodin.html">
 					<img src="images/oodin-thumbnail.jpg" alt="Square Point of Sale tablet & mobile design for Android">
 					<span>OODIN</span>
-				</a>
-				<a class="column">
 				</a>
 			</div>
 		</div>
