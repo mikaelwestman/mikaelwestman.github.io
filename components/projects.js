f0c51a5f0c1fa5998970381db3f65887156a4c1c
mikaelwestman <mikael@wst.mn>
2022-01-23 14:14:13 +0100

    updated styling

--- components/projects.js
+++ components/projects.js
@@ -10,31 +10,31 @@ class Projects extends HTMLElement {
 			<div class="row">
 				<a class="column" href="3d.html">
 					<img src="images/3d-lighter-mikael-westman.jpg" alt="3D render of lighter">
-					<span>3D explorations</span>
+					<span class="thumbnail-title">3D explorations</span>
 				</a>
 				<a class="column" href="epidemic-sound.html">
 	  				<img src="images/Epidemic-Sound-Artist.jpg" alt="Epidemic Sound artist page">
-					<span>Epidemic Sound</span>
+					<span class="thumbnail-title">Epidemic Sound</span>
 				</a>
 			</div>
 			<div class="row">
 				<a class="column" href="square-pos.html">
 					<img src="images/pos-mikael-westman-01.jpg" alt="Square Point of Sale tablet & mobile design for Android">
-					<span>Square Point of Sale</span>
+					<span class="thumbnail-title">Square Point of Sale</span>
 				</a>
 				<a class="column" href="bryggmastaren.html">
 					<img src="images/bryggmastarens-mikael-westman-03.png" alt="Square Point of Sale tablet & mobile design for Android">
-					<span>Bryggmästarens</span>
+					<span class="thumbnail-title">Bryggmästarens</span>
 				</a>
 			</div>
 				<div class="row">
 				<a class="column" href="square-for-restaurants.html">
 					<img src="images/square-mikael-westman-01.png" alt="Square Point of Sale tablet & mobile design for Android">
-					<span>Square for Restaurants</span>
+					<span class="thumbnail-title">Square for Restaurants</span>
 				</a>
 				<a class="column" href="oodin.html">
 					<img src="images/oodin-thumbnail.jpg" alt="Square Point of Sale tablet & mobile design for Android">
-					<span>OODIN</span>
+					<span class="thumbnail-title">OODIN</span>
 				</a>
 			</div>
 		</div>
