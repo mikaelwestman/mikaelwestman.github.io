33a90014f8ad572da18e852252430907b48e5492
mikaelwestman <mikael@wst.mn>
2022-01-24 12:33:41 +0100

    Update metadata.js

--- components/metadata.js
+++ components/metadata.js
@@ -9,7 +9,7 @@ class Meta extends HTMLElement {
 		<meta name="description" content="Portfolio of Mikael Westman, Product Designer at Epidemic Sound in Stockholm, previously at Square in New York City.">
 		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="icon" type="image/png" href="icon.png?v=1">
+		<link rel="icon" type="image/png" href="icon.png?">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
     `;
   }
