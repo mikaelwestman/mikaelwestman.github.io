bd7f33b169cb53b78b49c07362729798535462d0
mikaelwestman <mikael@wst.mn>
2022-03-25 22:10:17 +0100

    flags

--- components/projects.js
+++ components/projects.js
@@ -23,18 +23,24 @@ class Projects extends HTMLElement {
 					<span class="thumbnail-title">Square Point of Sale</span>
 				</a>
 				<a class="column" href="bryggmastaren.html">
-					<img src="images/bryggmastarens-mikael-westman-03.png" alt="Square Point of Sale tablet & mobile design for Android">
+					<img src="images/bryggmastarens-mikael-westman-03.png" alt="Bottlecap">
 					<span class="thumbnail-title">Bryggmästarens</span>
 				</a>
 			</div>
-				<div class="row">
+			<div class="row">
 				<a class="column" href="square-for-restaurants.html">
-					<img src="images/square-mikael-westman-01.png" alt="Square Point of Sale tablet & mobile design for Android">
+					<img src="images/square-mikael-westman-01.png" alt="Square for Restaurants">
 					<span class="thumbnail-title">Square for Restaurants</span>
 				</a>
 				<a class="column" href="oodin.html">
-					<img src="images/oodin-thumbnail.jpg" alt="Square Point of Sale tablet & mobile design for Android">
+					<img src="images/oodin-thumbnail.jpg" alt="OODIN logo">
 					<span class="thumbnail-title">OODIN</span>
+				</a>
+			</div>
+			<div class="row">
+				<a class="column" href="vexillography.html">
+					<img src="images/flag-uca-mikael-westman.jpg" alt="Flag">
+					<span class="thumbnail-title">Vexillography</span>
 				</a>
 			</div>
 		</div>
