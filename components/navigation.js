4455a3cd323be53924af500fc4f5d73c999def5f
mikaelwestman <mikael@wst.mn>
2023-01-03 10:17:34 +0100

    Update navigation.js

--- components/navigation.js
+++ components/navigation.js
@@ -7,7 +7,7 @@ class Navigation extends HTMLElement {
     this.innerHTML = `
 		<div id="top-navigation" class="row">
 			<div class="column">
-				<a href="/"><h1><span>© Mikael Westman 2022</span></h1></a>
+				<a href="/"><h1><span>© Mikael Westman 2023</span></h1></a>
 			</div>
 			<div class="column">	
 				<a href="/" id="menu-active-1">Work</a>
