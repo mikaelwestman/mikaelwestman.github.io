d67fcdf0144de51fcb002539ddbde0cfc9e94f6a
mikaelwestman <mikael@wst.mn>
2024-10-19 12:24:50 +0200

    PEL-I-CAN

--- components/projects.js
+++ components/projects.js
@@ -12,36 +12,42 @@ class Projects extends HTMLElement {
 					<div class="thumbnail-image-wrapper"><img src="images/stool-thumb.jpg" alt="Stool"></div>
 					<span class="thumbnail-title">The Ball Stool</span>
 				</a>
+				<a class="column" href="pelican.html">
+					<div class="thumbnail-image-wrapper"><img src="images/pelican-spoon-mikael-westman-02.jpg" alt="PEL-I-CAN spoon"></div>
+					<span class="thumbnail-title">Childrens tableware</span>
+				</a>
+			</div>
+			<div class="row">
 				<a class="column" href="3d.html">
 					<div class="thumbnail-image-wrapper"><img src="images/3d-lighter-mikael-westman.jpg" alt="3D render of lighter"></div>
 					<span class="thumbnail-title">3D</span>
 				</a>
-			</div>
-			<div class="row">
 				<a class="column" href="vexillography.html">
 					<div class="thumbnail-image-wrapper"><img src="images/flag-uca-mikael-westman.jpg" alt="Flag"></div>
 					<span class="thumbnail-title">Vexillography</span>
 				</a>
+			</div>
+			<div class="row">
 				<a class="column" href="variable-font.html">
 					<div class="thumbnail-image-wrapper"><img src="images/variable-font-thumb.png" alt="Variable font"></div>
 					<span class="thumbnail-title">Stretchy font</span>
 				</a>
-			</div>
-			<div class="row">
 				<a class="column" href="epidemic-sound.html">
 	  			<div class="thumbnail-image-wrapper"><img src="images/Epidemic-Sound-Artist.jpg" alt="Epidemic Sound artist page"></div>
 					<span class="thumbnail-title">Epidemic Sound</span>
 				</a>
+			</div>
+			<div class="row">
 				<a class="column" href="square-for-restaurants.html">
 					<div class="thumbnail-image-wrapper"><img src="images/square-mikael-westman-04.png" alt="Square for Restaurants"></div>
 					<span class="thumbnail-title">Square for Restaurants</span>
 				</a>
-			</div>
-			<div class="row">
 				<a class="column" href="square-pos.html">
 					<div class="thumbnail-image-wrapper"><img src="images/pos-mikael-westman-01.png" alt="Square Point of Sale tablet & mobile design for Android"></div>
 					<span class="thumbnail-title">Square Point of Sale</span>
 				</a>
+			</div>
+			<div class="row">
 				<a class="column" href="oodin.html">
 					<div class="thumbnail-image-wrapper"><img src="images/oodin-thumbnail.jpg" alt="OODIN logo"></div>
 					<span class="thumbnail-title">OODIN</span>
