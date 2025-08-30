d88932a474414cd98cdd5d2c9d76994272b3463c
mikaelwestman <mikael@wst.mn>
2025-08-30 18:28:38 +0200

    Update scripts.js

--- components/scripts.js
+++ components/scripts.js
@@ -2,7 +2,7 @@ class Scripts extends HTMLElement {
   connectedCallback() {
     // Create script elements for common components
     const scripts = [
-      { src: 'components/navigation.js', defer: true },
+      { src: 'components/navigation.js', defer: false }, // Load navigation synchronously to prevent blinking
       { src: 'components/footer.js', defer: true },
       { src: 'components/projects.js', defer: true },
       { src: 'components/loading-manager.js', defer: true },
